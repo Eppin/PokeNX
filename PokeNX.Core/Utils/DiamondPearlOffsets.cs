ca358e4dd6c6af4964e00b47e8d6237b4d36591d
Kevin Wilbrink <kevin@wilbrink.dev>
2021-12-20 18:27:25 +0100

    Removed test code

--- PokeNX.Core/Utils/DiamondPearlOffsets.cs
+++ PokeNX.Core/Utils/DiamondPearlOffsets.cs
@@ -9,7 +9,7 @@ namespace PokeNX.Core.Utils
 
         public const uint DiamondPlayerPrefsProviderInstance = 0x4c49098;
         public const uint DiamondFieldManager = 0x4c13650;
-        public const string DiamondBoxStartPointer = "[main+4E34DD0]";//"+B8+10+A0+20+20+20]"; // 0x4E34DD0, 0xB8, 0x10, 0xA0, 0x20, 0x20, 0x20
+        public const string DiamondBoxStartPointer = "[[[[main+4C1DCF8]+B8]+10]+A0]+20"; // v1.1.1 => [[[[main+4C1DCF8]+B8]+10]+A0]+20, v1.1.2 => [[[[main+4E34DD0]+B8]+10]+A0]+20
 
         public const uint PearlPlayerPrefsProviderInstance = 0x4e60170;
         public const uint PearlFieldManager = 0x4e2a728;
