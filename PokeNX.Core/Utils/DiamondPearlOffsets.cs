505c523993911dd7e283c9166239d0ed11c3311a
Kevin Wilbrink <kevin@wilbrink.dev>
2021-12-22 14:24:50 +0100

    Update offsets for v1.1.3

--- PokeNX.Core/Utils/DiamondPearlOffsets.cs
+++ PokeNX.Core/Utils/DiamondPearlOffsets.cs
@@ -5,8 +5,8 @@ namespace PokeNX.Core.Utils
         public const string ShiningPearlID = "010018E011D92000";
         public const string BrilliantDiamondID = "0100000011D90000";
 
-        public const string MainPointer = "[main+4F8CCD0]";
+        public const string MainPointer = "[main+4FB2050]";
 
-        public const uint PlayerPrefsProviderInstance = 0x4e60170;
+        public const uint PlayerPrefsProviderInstance = 0x4E853F0;
     }
 }
